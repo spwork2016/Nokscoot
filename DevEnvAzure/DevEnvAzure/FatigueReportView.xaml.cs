70bfcf3c3416b90206b6b046568b77e97c337ee2
uday b <uday377@gmail.com>
2018-04-01 19:07:18 +0530

    UI changes

--- DevEnvAzure/DevEnvAzure/FatigueReportView.xaml.cs
+++ DevEnvAzure/DevEnvAzure/FatigueReportView.xaml.cs
@@ -12,9 +12,18 @@ namespace DevEnvAzure
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FatigueReportView : ContentView
     {
+        public static string RatehowyoufeltValue;
+      //  Models.FatigueReport _FatigueReport;
         public FatigueReportView()
         {
             InitializeComponent();
+          //  _FatigueReport = new Models.FatigueReport();
+          //  _FatigueReport.localReportDate = DateTime.Now;
         }
+        private void Ratehowyoufelt_changed(object sender, EventArgs e)
+        {
+            RatehowyoufeltValue = Ratehowyoufeltpicker.Items.ElementAt(Ratehowyoufeltpicker.SelectedIndex);
+        }
+
     }
 }
