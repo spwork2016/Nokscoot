4dbfac4cdfc15a81cf2c74d26965ab24e1c1f2fd
Surya Pabbineedi <indian.surya@gmail.com>
2020-01-21 22:53:22 +0530

    Update Authenticator.cs

--- DevEnvAzure/DevEnvAzure.iOS/Authenticator.cs
+++ DevEnvAzure/DevEnvAzure.iOS/Authenticator.cs
@@ -1,65 +1,59 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
+using Foundation;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using UIKit;
 using Xamarin.Forms;
-using System.Threading.Tasks;
-using Foundation;
 
-[assembly: Dependency(typeof(DevEnvAzure.iOS.Authenticator))]
-namespace DevEnvAzure.iOS
-{
-    class Authenticator : IAuthenticator
-    {
-        public async Task<AuthenticationResult> Authenticate(string tenantUrl, string resourceUri, string ApplicationID, string returnUri)
-        {
-            try
-            {
-                var authContext = new AuthenticationContext(tenantUrl);
+[assembly : Dependency (typeof (DevEnvAzure.iOS.Authenticator))]
+namespace DevEnvAzure.iOS {
+	class Authenticator : IAuthenticator {
+		private async Task<AuthenticationResult> AcquireTokenAsync (AuthenticationContext ac, string tenantUrl, string resourceUri, string ApplicationID, string returnUri) {
+			return await ac.AcquireTokenAsync (resourceUri, ApplicationID, new Uri (returnUri),
+				new PlatformParameters (UIApplication.SharedApplication.KeyWindow.RootViewController));
+		}
 
-                // Make sure this pre-compilation constant exists in your enviroment!
-#if __IOS__
-                authContext.iOSKeychainSecurityGroup = "com.microsoft.adalcache";
-#endif
+		public async Task<AuthenticationResult> Authenticate (string tenantUrl, string resourceUri, string ApplicationID, string returnUri) {
+			var ac = new AuthenticationContext (tenantUrl);
+			AuthenticationResult result = null;
 
-                if (authContext.TokenCache.ReadItems().Any())
-                    authContext = new AuthenticationContext(authContext.TokenCache.ReadItems().FirstOrDefault().Authority);
+			try {
+				result = await ac.AcquireTokenSilentAsync (resourceUri, ApplicationID);
 
-                var authResult = await authContext.AcquireTokenAsync(resourceUri, ApplicationID, new Uri(returnUri),
-                    new PlatformParameters(UIApplication.SharedApplication.KeyWindow.RootViewController));
+				if (result == null) {
+					result = await AcquireTokenAsync (ac, tenantUrl, resourceUri, ApplicationID, returnUri);
+				}
+			} catch (AdalException adalException) {
+				if (adalException.ErrorCode == AdalError.FailedToAcquireTokenSilently ||
+					adalException.ErrorCode == AdalError.InteractionRequired) {
+					result = await AcquireTokenAsync (ac, tenantUrl, resourceUri, ApplicationID, returnUri);
+				}
+			} catch (Exception ex) {
+				return null;
+			}
 
-                return authResult;
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
+			return result;
+		}
 
-        public void Logout()
-        {
-            NSHttpCookieStorage CookieStorage = NSHttpCookieStorage.SharedStorage;
-            foreach (var cookie in CookieStorage.Cookies)
-            {
-                CookieStorage.DeleteCookie(cookie);
-            }
-        }
+		public void Logout () {
+			NSHttpCookieStorage CookieStorage = NSHttpCookieStorage.SharedStorage;
+			foreach (var cookie in CookieStorage.Cookies) {
+				CookieStorage.DeleteCookie (cookie);
+			}
+		}
 
-        public async Task<AuthenticationResult> ReAuthenticate(string tenantUrl, string graphResourceUri, string ApplicationID, string returnUri)
-        {
-            try
-            {
-                var authContext = new AuthenticationContext(tenantUrl);
-                if (authContext.TokenCache.ReadItems().Any())
-                    authContext = new AuthenticationContext(authContext.TokenCache.ReadItems().FirstOrDefault().Authority);
-                var authResult = await authContext.AcquireTokenAsync(graphResourceUri, ApplicationID, new Uri(returnUri),
-                    new PlatformParameters(UIApplication.SharedApplication.KeyWindow.RootViewController));
-                return authResult;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-    }
+		public async Task<AuthenticationResult> ReAuthenticate (string tenantUrl, string graphResourceUri, string ApplicationID, string returnUri) {
+			try {
+				var authContext = new AuthenticationContext (tenantUrl);
+				if (authContext.TokenCache.ReadItems ().Any ())
+					authContext = new AuthenticationContext (authContext.TokenCache.ReadItems ().FirstOrDefault ().Authority);
+				var authResult = await authContext.AcquireTokenAsync (graphResourceUri, ApplicationID, new Uri (returnUri),
+					new PlatformParameters (UIApplication.SharedApplication.KeyWindow.RootViewController));
+				return authResult;
+			} catch (Exception) {
+				return null;
+			}
+		}
+	}
 }
